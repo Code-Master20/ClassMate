602d7e23c2ef604460cda7a555e4d93a27b45101
Code-Master20 <sahidurmiah201920@gmail.com>
2026-02-06 01:03:54 +0530

    userSchema updated with more fields

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,6 +10,49 @@ const userSchema = new mongoose.Schema(
       trim: true,
       lowercase: true,
     },
+
+    bio: {
+      type: [String], // <-- Array of strings
+      trim: true,
+    },
+
+    status: {
+      //maried, unmarried, single, relationships, divorced, etc
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+
+    gender: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+
+    dob: {
+      tupe: String,
+    },
+
+    talent: {
+      type: [String],
+      trim: true,
+      lowercase: true,
+    },
+
+    profession: {
+      type: String, //doctor, teacher, nurse, police, army, cifs, bsf, software-developer, etc
+      trim: true,
+      lowercase: true,
+    },
+
+    avatar: {
+      type: String,
+      trim: true,
+    },
+    banner: {
+      type: String,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
